42ee1c83a0c3d8da833718454257999ff97a05c2
HyeonAm <oha949500@gmail.com>
2024-02-03 22:08:23 +0900

    JFrame Swing UI 설계및 로직 수정 v.1.2

--- src/Drinks.java
+++ src/Drinks.java
@@ -2,8 +2,6 @@ public class Drinks extends Vender{
     private String name;
     private String type;
 
-    private int ml;
-
     public String getName() {
         return name;
     }
@@ -19,20 +17,6 @@ public class Drinks extends Vender{
     public void setType(String type) {
         this.type = type;
     }
-
-    public int getMl() {
-        return ml;
-    }
-
-    public void setMl(int ml) {
-        this.ml = ml;
-    }
-
-
-    public void outPut(String name){
-        System.out.println("음료 "+name+"가 나왔습니다");
-    }
-
     public void stock(String name, int price){
         this.name = name;
         this.setPrice(price);
--- src/VandingMachine.java
+++ src/VandingMachine.java
@@ -1,8 +1,9 @@
+import javax.swing.*;
+
 public class VandingMachine {
     public static void main(String[] args) {
-        VendingFrame vendingFrame = new VendingFrame();
-        vendingFrame.setSize(500, 600);
-        vendingFrame.setVisible(true);
-
+        VendingFrame v = new VendingFrame();
+        v.setSize(500, 600);
+        v.setVisible(true);
     }
 }
